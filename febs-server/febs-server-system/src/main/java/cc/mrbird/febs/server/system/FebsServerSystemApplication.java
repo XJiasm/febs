a966bcfebee09cc09ee9d905df88fc06dd36ed33
mrbird <852252810@qq.com>
2019-10-21 09:35:19 +0800

    去除不必要注解

--- febs-server/febs-server-system/src/main/java/cc/mrbird/febs/server/system/FebsServerSystemApplication.java
+++ febs-server/febs-server-system/src/main/java/cc/mrbird/febs/server/system/FebsServerSystemApplication.java
@@ -4,13 +4,11 @@ import cc.mrbird.febs.common.annotation.FebsCloudApplication;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @EnableAsync
-@EnableDiscoveryClient
 @SpringBootApplication
 @FebsCloudApplication
 @EnableTransactionManagement
--- febs-server/febs-server-test/src/main/java/cc/mrbird/febs/server/test/FebsServerTestApplication.java
+++ febs-server/febs-server-test/src/main/java/cc/mrbird/febs/server/test/FebsServerTestApplication.java
@@ -3,12 +3,10 @@ package cc.mrbird.febs.server.test;
 import cc.mrbird.febs.common.annotation.FebsCloudApplication;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 
 @EnableFeignClients
-@EnableDiscoveryClient
 @FebsCloudApplication
 @SpringBootApplication
 @EnableGlobalMethodSecurity(prePostEnabled = true)
