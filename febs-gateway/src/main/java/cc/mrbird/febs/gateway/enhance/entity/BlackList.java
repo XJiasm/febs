52b10a90346c94e8e35604465513a787e2d6aff2
MrBird <852252810@qq.com>
2020-09-03 15:46:52 +0800

    fix

--- febs-gateway/src/main/java/cc/mrbird/febs/gateway/enhance/entity/BlackList.java
+++ febs-gateway/src/main/java/cc/mrbird/febs/gateway/enhance/entity/BlackList.java
@@ -17,8 +17,8 @@ import org.springframework.data.mongodb.core.mapping.Document;
 @AllArgsConstructor
 public class BlackList {
 
-    public static final String CLOSE = "0";
-    public static final String OPEN = "1";
+    public static final int CLOSE = 0;
+    public static final int OPEN = 1;
 
     public static final String METHOD_ALL = "all";
 
@@ -51,7 +51,7 @@ public class BlackList {
     /**
      * 状态，0关闭，1开启
      */
-    private Integer status;
+    private String status;
     /**
      * 规则创建时间
      */
--- febs-gateway/src/main/java/cc/mrbird/febs/gateway/enhance/entity/RateLimitRule.java
+++ febs-gateway/src/main/java/cc/mrbird/febs/gateway/enhance/entity/RateLimitRule.java
@@ -18,8 +18,8 @@ import org.springframework.data.mongodb.core.mapping.Document;
 @AllArgsConstructor
 public class RateLimitRule {
 
-    public static final String CLOSE = "0";
-    public static final String OPEN = "1";
+    public static final int CLOSE = 0;
+    public static final int OPEN = 1;
 
     public static final String METHOD_ALL = "all";
 
@@ -52,7 +52,7 @@ public class RateLimitRule {
     /**
      * 状态，0关闭，1开启
      */
-    private Integer status;
+    private String status;
     /**
      * 规则创建时间
      */
